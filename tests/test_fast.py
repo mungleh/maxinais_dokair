561c1643cfed1a2b8ce50da5c0bd5bcb2a09d038
maxime lu <merdeth69@gmail.com>
2023-08-02 15:22:15 +0200

    pls

--- tests/test_fast.py
+++ tests/test_fast.py
@@ -8,3 +8,5 @@ class TestIrisModel(unittest.TestCase):
    def test_model_initialization(self):
        new_model = IrisModel()
        self.assertIn('iris_model.pkl',new_model.model_fname_)
+
+#lel
